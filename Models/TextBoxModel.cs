c9ca37b882f0ed12c3cfb9e509fd96133ef3db1e
Qapples <armand.scotty@yahoo.com>
2020-02-09 20:03:37 -0500

    Add

---
+++ Models/TextBoxModel.cs
@@ -0,0 +1,51 @@
+﻿using System.Runtime.CompilerServices;
+using System.ComponentModel;
+
+namespace Smash64FileAppender.Models
+{
+    public class TextBoxModel : INotifyPropertyChanged
+    {
+        string _inputText;
+        string _offsetText;
+
+        
+        /// <summary>
+        /// Text of the input.
+        /// </summary>
+        public string InputText
+        {
+            get => _inputText;
+            set
+            {
+                if (value != _inputText)
+                {
+                    _inputText = value;
+                    OnPropertyChanged(nameof(InputText));
+                }
+            }
+        }
+        
+        /// <summary>
+        /// Text Offset
+        /// </summary>
+        public string OffsetText
+        {
+            get => _offsetText;
+            set
+            {
+                if (value != _offsetText)
+                {
+                    _offsetText = value;
+                    OnPropertyChanged(nameof(OffsetText));
+                }
+            }
+        }
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,117 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using Smash64FileAppender.Models;
+
+namespace Smash64FileAppender.Views
+{
+    public class MainWindow : Window
+    {
+        List<byte> _bytes = new List<byte>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new TextBoxModel() {InputText = "", OffsetText = ""};
+            ClientSize = new Size(500, 500);
+        }
+
+        public async void ImportClick(object sender, RoutedEventArgs e)
+        {
+            //Dialog
+            OpenFileDialog dialog = new OpenFileDialog();
+            FileDialogFilter filter = new FileDialogFilter
+            {
+                Extensions = new[] {"bin"}.ToList(), Name = ".bin file"
+            };
+
+            List<FileDialogFilter> list = new List<FileDialogFilter> {filter};
+            dialog.Filters = list;
+
+            string[] fileNames = await dialog.ShowAsync(this);
+            string output = "";
+
+            int prevLength = _bytes.Count;
+            int firstPointerIndex = 0;
+
+            //Read bytes and change output
+            List<byte> addBytes = (await File.ReadAllBytesAsync(fileNames[0])).ToList();
+            _bytes.AddRange(addBytes);
+
+            //Last pointer is USUALLY the last two bytes (minus the 15 0's)
+            int lastPointerIndex = _bytes[^19];
+
+            //Update the pointer bytes. If there are already bytes, then search for the first pointer.
+            if (prevLength > 0)
+            {
+                for (int i = prevLength; i < _bytes.Count - 1 - prevLength; i += 2)
+                {
+                    //If these two bytes are equal to the start of the file (8), then we found the pointer.
+                    if (((_bytes[i] << 8) + _bytes[i + 1]) * 4 + prevLength == prevLength + 8)
+                    {
+                        firstPointerIndex = i - 2;
+                        break;
+                    }
+                }
+                //Follow the pointers and update them accordingly
+                await EditPointers(firstPointerIndex, prevLength);
+            }
+
+            Console.WriteLine("\n" + firstPointerIndex.ToString("X"));
+
+            //string output = _bytes.Aggregate("", (current, b) => current + b.ToString("X2"));
+
+            ((TextBoxModel) DataContext).InputText +=
+                addBytes.Aggregate("", (current, b) => current + b.ToString("X2"));
+        }
+
+        public Task EditPointers(int index, int length)
+        {
+            while (_bytes[index] != 0xFF)
+            {
+                int pointerValue = ((_bytes[index] << 8) + _bytes[index + 1]);
+                _bytes[index] = (byte) ((pointerValue + length) << 8);
+                _bytes[index + 1] = (byte) (pointerValue + length);
+                index = pointerValue * 4;
+                Console.WriteLine(index.ToString("X"));
+            }
+
+            return Task.CompletedTask; 
+        }
+
+        public async void ExportClick(object sender, RoutedEventArgs e)
+        {
+            TextBoxModel context = DataContext as TextBoxModel;
+
+            //Dialog
+            SaveFileDialog dialog = new SaveFileDialog();
+            FileDialogFilter filter = new FileDialogFilter
+            {
+                Extensions = new string[] {"bin",}.ToList(), Name = "bin files"
+            };
+            List<FileDialogFilter> list = new List<FileDialogFilter> {filter};
+            dialog.Filters = list;
+
+            string fileName = await dialog.ShowAsync(this);
+
+            await using StreamWriter writer = new StreamWriter(fileName);
+            for (int i = 0; i < context.InputText.Length - 1; i += 2)
+            {
+                await writer.WriteAsync((char) Convert.ToByte($"{context.InputText[i]}{context.InputText[i + 1]}", 16));
+            }
+
+            await writer.FlushAsync();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
